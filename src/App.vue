294be50b52a84a5496fc10bfa75c126cf22bb2de
Jingwen Li <865581101@qq.com>
2022-03-06 13:30:48 +0100

    full implementation?

--- src/App.vue
+++ src/App.vue
@@ -20,15 +20,17 @@
 
 		<AwaitIteratedResultScreen :title="'Waiting for previous participant to finish'"></AwaitIteratedResultScreen>
 
-		<!-- Round 1: halved_seen and halved_unseen -->
-		<InstructionScreen :title="'Training Round 1a'">
+		<!-- Train on randomised full SEEN set -->
+		<Screen :title="'Training Round 1a'">
 			When you are ready, click the button to start your training.
+			<!-- for debugging -->
 			<p> Chain: {{$magpie.socket.chain}} </p>
 			<p> Generation: {{$magpie.socket.generation}} </p>
-			<p> Last iteration results: {{$magpie.socket.lastIterationResults}} </p>
-			<p>{{getPreviousResponse()}}</p>
+			<button @click="getPreviousResponse();$magpie.nextScreen();">
+				Next
+			</button>
 
-		</InstructionScreen>
+		</Screen>
 
 		<template v-for="(index, i) of train1a">
 			<Screen :key = "i">
@@ -39,12 +41,16 @@
 				<Slide> <!-- Display picture and label for 5 seconds -->
 					{{vocab[index]}}
 					<br/>
-					<img :src="pictures[index].pic" />
+<!-- 					{{index}}
+					{{pics[index]}} -->
+					<img :src="pics[index].pic" />
 					<Record :data="{
+						chain: $magpie.socket.chain,
+						generation: $magpie.socket.generation,
 						trial_type: 'training 1a',
 						trial_number: i+1,
 						expected: vocab[index],
-						item: pictures[index].item,
+						item: pics[index].item,
 					}"/>
 					<Wait :time="5000" @done= "$magpie.nextSlide()" />
 <!--          <Wait :time="5000" @done= "$magpie.saveAndnextScreen()" /> -->
@@ -56,7 +62,7 @@
 				</Slide>
 			</Screen>
 		</template>
-
+		<!-- Train on randomised full SEEN set -->
 		<InstructionScreen :title="'Training Round 1b'">
 			When you are ready, click the button to start your next training session.
 
@@ -71,12 +77,14 @@
 				<Slide> <!-- Display picture and label for 5 seconds -->
 					{{vocab[index]}}
 					<br/>
-					<img :src="pictures[index].pic" />
+					<img :src="pics[index].pic" />
 					<Record :data="{
+						chain: $magpie.socket.chain,
+						generation: $magpie.socket.generation,
 						trial_type: 'training 1b',
 						trial_number: i+1,
 						expected: vocab[index],
-						item: pictures[index].item,
+						item: pics[index].item,
 					}"/>
 					<Wait :time="5000" @done= "$magpie.nextSlide()" />
 <!--          <Wait :time="5000" @done= "$magpie.saveAndnextScreen()" /> -->
@@ -88,7 +96,7 @@
 				</Slide>
 			</Screen>
 		</template>
-
+		<!-- Test on randomised halved SEEN and halved UNSEEN -->
 		<InstructionScreen :title="'Testing Round 1'">
 			When you are ready, click the button to start your test for training round 1.
 
@@ -97,14 +105,16 @@
 		<template v-for="(index, i) of test1">
 			<Screen :key = "i">
 				<Slide>
-					<img :src="pictures[index].pic" />
+					<img :src="pics[index].pic" />
 					<TextareaInput :response.sync="$magpie.measurements.input" />
 					<button @click="$magpie.saveAndNextScreen();">Next</button>
 					<Record :data="{
+						chain: $magpie.socket.chain,
+						generation: $magpie.socket.generation,
 						trial_type: 'testing 1',
 						trial_number: i+1,
 						expected: vocab[index],
-						item: pictures[index].item,
+						item: pics[index].item,
 					}"/>
 				</Slide>
 			</Screen>
@@ -124,12 +134,14 @@
 				<Slide> <!-- Display picture and label for 5 seconds -->
 					{{vocab[index]}}
 					<br/>
-					<img :src="pictures[index].pic" />
+					<img :src="pics[index].pic" />
 					<Record :data="{
+						chain: $magpie.socket.chain,
+						generation: $magpie.socket.generation,
 						trial_type: 'training 2a',
 						trial_number: i+1,
 						expected: vocab[index],
-						item: pictures[index].item,
+						item: pics[index].item,
 					}"/>
 					<Wait :time="5000" @done= "$magpie.nextSlide()" />
 <!--          <Wait :time="5000" @done= "$magpie.saveAndnextScreen()" /> -->
@@ -156,12 +168,14 @@
 				<Slide> <!-- Display picture and label for 5 seconds -->
 					{{vocab[index]}}
 					<br/>
-					<img :src="pictures[index].pic" />
+					<img :src="pics[index].pic" />
 					<Record :data="{
+						chain: $magpie.socket.chain,
+						generation: $magpie.socket.generation,
 						trial_type: 'training 2b',
 						trial_number: i+1,
 						expected: vocab[index],
-						item: pictures[index].item,
+						item: pics[index].item,
 					}"/>
 					<Wait :time="5000" @done= "$magpie.nextSlide()" />
 <!--          <Wait :time="5000" @done= "$magpie.saveAndnextScreen()" /> -->
@@ -182,19 +196,88 @@
 		<template v-for="(index, i) of test2">
 			<Screen :key = "i">
 				<Slide>
-					<img :src="pictures[index].pic" />
+					<img :src="pics[index].pic" />
 					<TextareaInput :response.sync="$magpie.measurements.input" />
 					<button @click="$magpie.saveAndNextScreen();">Next</button>
 					<Record :data="{
+						chain: $magpie.socket.chain,
+						generation: $magpie.socket.generation,
 						trial_type: 'testing 2',
 						trial_number: i+1,
 						expected: vocab[index],
-						item: pictures[index].item,
+						item: pics[index].item,
 					}"/>
 				</Slide>
 			</Screen>
 		</template>
 		<!-- Round 3: seen and unseen -->
+		<InstructionScreen :title="'Training Round 3a'">
+			When you are ready, click the button to start your second round of training.
+
+		</InstructionScreen>
+
+		<template v-for="(index, i) of train3a">
+			<Screen :key = "i">
+				<Slide> <!-- Display label for 1 second -->
+					{{vocab[index]}}
+					<Wait :time="1000" @done= "$magpie.nextSlide()" />
+				</Slide>
+				<Slide> <!-- Display picture and label for 5 seconds -->
+					{{vocab[index]}}
+					<br/>
+					<img :src="pics[index].pic" />
+					<Record :data="{
+						chain: $magpie.socket.chain,
+						generation: $magpie.socket.generation,
+						trial_type: 'training 3a',
+						trial_number: i+1,
+						expected: vocab[index],
+						item: pics[index].item,
+					}"/>
+					<Wait :time="5000" @done= "$magpie.nextSlide()" />
+<!--          <Wait :time="5000" @done= "$magpie.saveAndnextScreen()" /> -->
+				</Slide>
+				<Slide> <!-- Optional manual transition -->
+					<button
+						v-if="!$magpie.measurements.hasOwnProperty('response')"
+						@click="$magpie.saveAndNextScreen();">Next</button>
+				</Slide>
+			</Screen>
+		</template>
+
+		<InstructionScreen :title="'Training Round 3b'">
+			When you are ready, click the button to start your next training session.
+
+		</InstructionScreen>
+
+		<template v-for="(index, i) of train3b">
+			<Screen :key = "i">
+				<Slide> <!-- Display label for 1 second -->
+					{{vocab[index]}}
+					<Wait :time="1000" @done= "$magpie.nextSlide()" />
+				</Slide>
+				<Slide> <!-- Display picture and label for 5 seconds -->
+					{{vocab[index]}}
+					<br/>
+					<img :src="pics[index].pic" />
+					<Record :data="{
+						chain: $magpie.socket.chain,
+						generation: $magpie.socket.generation,
+						trial_type: 'training 3b',
+						trial_number: i+1,
+						expected: vocab[index],
+						item: pics[index].item,
+					}"/>
+					<Wait :time="5000" @done= "$magpie.nextSlide()" />
+<!--          <Wait :time="5000" @done= "$magpie.saveAndnextScreen()" /> -->
+				</Slide>
+				<Slide> <!-- Optional manual transition -->
+					<button
+						v-if="!$magpie.measurements.hasOwnProperty('response')"
+						@click="$magpie.saveAndNextScreen();">Next</button>
+				</Slide>
+			</Screen>
+		</template>
 		<InstructionScreen :title="'Final Testing'">
 			When you are ready, click the button to start your final testing for the language.
 
@@ -203,19 +286,21 @@
 		<template v-for="(index, i) of final">
 			<Screen :key = "i">
 				<Slide>
-					<img :src="pictures[index].pic" />
+					<img :src="pics[index].pic" />
 					<TextareaInput :response.sync="$magpie.measurements.input" />
 					<button @click="$magpie.saveAndNextScreen();">Next</button>
 					<Record :data="{
+						chain: $magpie.socket.chain,
+						generation: $magpie.socket.generation,
 						trial_type: 'final',
 						trial_number: i+1,
 						expected: vocab[index],
-						item: pictures[index].item,
+						item: pics[index].item,
 					}"/>
 				</Slide>
 			</Screen>
 		</template>
-<!--    <PostTestScreen /> -->
+	 <PostTestScreen />
 
 		<!-- While developing your experiment, using the DebugResults screen is fine,
 			once you're going live, you can use the <SubmitResults> screen to automatically send your experimental data to the server. -->
@@ -234,6 +319,8 @@ import _shuffle from 'lodash/shuffle';
 import _difference from 'lodash/difference';
 import _slice from 'lodash/slice';
 import _filter from 'lodash/filter';
+import _uniq from 'lodash/uniq';
+import _at from 'lodash/at';
 
 export default {
 	name: 'App',
@@ -249,6 +336,8 @@ export default {
 		let train2a = [];
 		let train2b = [];
 		let test2 = [];
+		let train3a = [];
+		let train3b = [];
 		let final = [];
 
 		return {
@@ -263,6 +352,8 @@ export default {
 			train2a,
 			train2b,
 			test2,
+			train3a,
+			train3b,
 			final,
 		};
 	},
@@ -323,8 +414,27 @@ export default {
 		 },
 
 		filterAmbiguous:
-		 function(){// set this.vocab to unique, get corresponding indices
+		 function(){
+		  // set this.vocab to unique, get corresponding indices
+		  let unique_vocab = _uniq(this.vocab)
+		  console.log(unique_vocab)
+		  let indices = Array.from(unique_vocab, (el, _) => this.vocab.indexOf(el))
+		  console.log(this.vocab)
+		  console.log(indices)
 			// reset seen and unseen
+			console.log(indices.length)
+			if (indices.length < num_seen){  // if too many ambiguous words
+				num_seen = indices.length
+				num_train = Math.ceil(num_seen / 2)
+				num_test = Math.ceil((vocab_size - num_seen) / 2)
+			}
+			let sampled_unique_vocab_indices = this.sample_n(num_seen, indices)
+			this.seen = _at(indices, sampled_unique_vocab_indices)
+			this.unseen = _shuffle(_difference(Array.from(this.pictures, (_, idx) => idx), this.seen))
+			// prepare data for each round
+			this.prepareRound1();
+			this.prepareRound2();
+			this.prepareRound3();
 		 },
 
 		splitSeenUnseen:
@@ -336,20 +446,22 @@ export default {
 
 		prepareRound1:
 		 function(){
-			this.train1a = _slice(_shuffle(this.seen), num_train)
+			this.train1a = _shuffle(this.seen)
 			this.train1b = _shuffle(this.train1a)
-			this.test1 = _slice(_shuffle(this.unseen), num_test)
+			this.test1 = _shuffle(_slice(_shuffle(this.unseen), num_test).concat(_slice(_shuffle(this.seen), num_train)))
 		 },
 
 		prepareRound2:
 		 function(){
-			this.train2a = _slice(_shuffle(this.seen), num_train)
+			this.train2a = _shuffle(this.seen)
 			this.train2b = _shuffle(this.train2a)
-			this.test2 = _slice(_shuffle(this.unseen), num_test)
+			this.test2 = _shuffle(_slice(_shuffle(this.unseen), num_test).concat(_slice(_shuffle(this.seen), num_train)))
 		 },
 
 		prepareRound3:
 		 function(){
+		 	this.train3a = _shuffle(this.seen)
+			this.train3b = _shuffle(this.train3a)
 			this.final = this.randomiseData(pictures)
 		 },
 
@@ -367,27 +479,22 @@ export default {
 			if (generation == null){
 				return "empty socket";
 			}
-			if (generation == 1){
-				// do nothing in particular (experiment is ready)
-				return 0; // for testing
-			} else{
+			if (generation != 1){
 				// retrieve data from previous iteration
 				let lastIterationResults = this.$magpie.socket.lastIterationResults;
 				console.log(lastIterationResults)
 				// only data from the final round is relevant
 				let filteredData = _filter(lastIterationResults, ['trial_type', 'final'])
 				console.log(filteredData)
-				// console.log(filteredData.map(datapoint => datapoint.item))
-				// console.log(filteredData.map(datapoint => datapoint.input))
 
 				// replace pics and vocab with previous results
-				this.pics= filteredData.map(datapoint => datapoint.item)
+				let items = filteredData.map(datapoint => datapoint.item - 1)
+				this.pics = _at(this.pictures, items)
 				this.vocab = filteredData.map(datapoint => datapoint.input)
-				// filterAmbiguous (easy to implement once the previous step is done)
-				//
-				return -1;
+				// filter
+				this.filterAmbiguous();
 			}
-		 }
+		}
 	}
 };
 
@@ -395,8 +502,8 @@ const syllables = ["ma", "ni", "so", "ru", "ka", "gu", "ti", "de", "pa"];
 const vocab_size = 27;
 const min_syllables = 2;
 const max_syllables = 4;
-const num_seen = 14;
-const num_train = 7;
-const num_test = 7;
+let num_seen = Math.ceil(vocab_size / 2);
+let num_train = Math.ceil(num_seen / 2);
+let num_test = Math.ceil((vocab_size - num_seen) / 2);
 
 </script>
